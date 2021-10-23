ee0d341ed9fb90c56b96069b59d9b3c7deeed5ed
onodera_sf <sf_yo_service_yellow@outlook.jp>
2021-10-23 11:35:33 +0900

    プロジェクト ファイルを追加します。

---
+++ AspNetCoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using AspNetCoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMvc.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		// 追加
+		public IActionResult Blazor1()
+		{
+			return View();
+		}
+
+		public IActionResult Blazor2()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
